d35c94097ae7c36ffbb2a2c80caba826710ea49f
yamamotokaito0730 <149446520+yamamotokaito0730@users.noreply.github.com>
2025-07-17 09:22:03 +0900

    バグ解消

--- CHAINRUSH/Assets/Script/Enemy/Enemy.cs
+++ CHAINRUSH/Assets/Script/Enemy/Enemy.cs
@@ -29,7 +29,7 @@ public class Enemy : MonoBehaviour
     [SerializeField, Tooltip("パーツ（0:頭, 1:胴体, 2:手, 3:足）")] private GameObject[] m_Parts;  // 頭・胴体・手・足を配列で管理
     [SerializeField, Tooltip("生成数")] private int m_nPartsNum;    // オブジェクトの生成
 
-    public GameManager gamemanager;
+    //public GameManager gamemanager;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
