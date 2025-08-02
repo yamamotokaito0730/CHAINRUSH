aeb931f5efd0c542bb50c3899b9c2f5149b9e592
ToyamaRyota <tooyamaryouta@gmail.com>
2025-08-03 00:12:48 +0900

    タイトル、リザルト、ゲームオーバーシーンのpad対応

--- CHAINRUSH/Assets/Script/TItle/TItle.cs
+++ CHAINRUSH/Assets/Script/TItle/TItle.cs
@@ -17,24 +17,32 @@ _M06
 __D
 ___17:シーン切り替えに1.5秒遅延させるように変更
 ___20:SE追加:nakashima
+_M08
+__D
+___03:パッドに対応:tooyama
 =====*/
 
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
 public class Title : MonoBehaviour
 {
     private bool isLoading = false;  // 2度押し防止用
     static public bool ToTitle = false;
 
+    private Gamepad gamepad; // ゲームパッドを使えるように宣言
+
     private void Awake()
     {
-        BGMManager.Instance.PlayBGMWithFade("Title", 1f);    
+        BGMManager.Instance.PlayBGMWithFade("Title", 1f);
+        gamepad = Gamepad.current; // 毎フレーム更新
     }
 
     void Update()
     {
-        if (!isLoading && Input.GetKeyDown(KeyCode.Return))
+        gamepad = Gamepad.current; // 毎フレーム更新
+        if (!isLoading && (Input.GetKeyDown(KeyCode.Return) || (gamepad != null && gamepad.aButton.wasPressedThisFrame)))
         {
             //SE////////////////////////////////////////
             SEManager.Instance.Play("chainsaw");
--- CHAINRUSH/Assets/Script/TItle/TItlePlayer.cs
+++ CHAINRUSH/Assets/Script/TItle/TItlePlayer.cs
@@ -13,10 +13,14 @@ Y25
 _M06
 __D  
 ___06:プログラム作成:mori
+_M08
+__D
+___03:パッドに対応:tooyama
 
 =====*/
 
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class TitlePlayer : MonoBehaviour
 {
@@ -24,6 +28,7 @@ public class TitlePlayer : MonoBehaviour
     public float moveSpeed;
     private bool isMoving = false;
     private float posY;
+    private Gamepad gamepad; // ゲームパッドを使えるように宣言
 
     void Awake()
     {
@@ -39,13 +44,15 @@ public class TitlePlayer : MonoBehaviour
         animator.Play("Run_2", 0, 0.0f); // 第2引数: Layer、第3引数: 時間（0秒から）
         animator.Update(0f);           // 再度更新して即座に反映させる
         posY = transform.position.y;
+        gamepad = Gamepad.current;
     }
 
     // Update is called once per frame
     void Update()
     {
+        gamepad = Gamepad.current; // 毎フレーム更新
         // エンターキーを押したら移動開始
-        if (Input.GetKeyDown(KeyCode.Return))
+        if (Input.GetKeyDown(KeyCode.Return) || (gamepad != null && gamepad.aButton.wasPressedThisFrame))
         {
             isMoving = true;
         }
